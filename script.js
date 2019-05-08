001757b28931a8622bbd676f94a99267639ebeb2
majkel3321 <majkel3321@gmail.com>
2019-05-08 19:52:12 +0200

    V2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://immense-basin-49421.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://immense-basin-49421.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://immense-basin-49421.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
